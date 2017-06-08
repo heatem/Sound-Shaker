87c93a563b33e29223cd24a1b561c67961aabf02
Heather Mason <heatem@gmail.com>
2017-06-07 19:13:27 -0700

    Initial Commit

---
+++ Sound Shaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Shaker
+//
+//  Created by Heather Mason on 6/7/17.
+//  Copyright © 2017 HApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
